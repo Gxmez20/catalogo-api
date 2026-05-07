8931a0071bbab80f541e4a1ed2e0782818705121
Gxmez20 <kg1389608@gmail.com>
2026-05-06 22:19:55 -0500

    corregir: restaurar productos.service y limpiar archivos innecesarios

--- src/productos/productos.service.ts
+++ src/productos/productos.service.ts
@@ -1,4 +1,4 @@
-import { Injectable, NotFoundException, ConflictException, BadRequestException } from '@nestjs/common';
+import { Injectable, NotFoundException, ConflictException } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
 import { Producto, ProductoDocument } from './productos.schema';
@@ -8,6 +8,7 @@ import { CategoriasService } from '../categorias/categorias.service';
 
 @Injectable()
 export class ProductosService {
+
   constructor(
     @InjectModel(Producto.name) private productoModel: Model<ProductoDocument>,
     private readonly categoriasService: CategoriasService,
